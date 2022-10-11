fdb8a6bb377b1fa2ca9e57199e581918789bb056
prairieson <114734911+prairieson@users.noreply.github.com>
2022-10-11 10:38:41 +0300

    Revert "New version Bot for Python"

--- main.py
+++ main.py
@@ -1,91 +1,40 @@
 """
  {Вопрос на входе} => {Алгоритм ответа} => {Ответ на выходе}
  Простой алгоритм – простой поиск по базе известных вопросов и ответов
-"""
-import urllib.request  # Для работы со ссылками
-import json  # Для работы с json
-import random
-from sklearn.feature_extraction.text import CountVectorizer  # Для обучения векторайзера (простенького ML)
-from sklearn.neural_network import MLPClassifier
-
-"""
-ШАГ 1. Собираем данные, на основании которых будем обучать.
-
-1. Фраза на вход => Модель предсказывает интент фразы.
-2. Входные данные (Фразы, X) Выходные данные (Интенты, y).
-3. Модель обучится на наших примерах и сможет предсказывать интенты по фразе.
-"""
-# Скачаем файл с большим количеством примеров для обучения
-url = "https://drive.google.com/uc?export=download&id=1VyvE5DGPqWFBHm5MFPUv-upq-SMZEXHx"
-filename = "local_intents_dataset.json"
-urllib.request.urlretrieve(url, filename)
-
-# Считываем файл в словарь
-with open(filename, 'r', encoding='UTF-8') as file:
-    data = json.load(file)
+ """
+ import urllib.request  # Для работы со ссылками
+ import json  # Для работы с json
+ from sklearn.feature_extraction.text import CountVectorizer  # Для обучения векторайзера (простенького ML)
 
 
-"""
-ШАГ 2. Преобразуем слова в числа, набор фраз в вектор.
-
-1. На вход подается большой набор фраз.
-2. Векторайзер обучается (каждому слову выделяется отдельная ячейка, 
-    куда будет вставляться код - число, сколько раз слово встретилось во фразе).
-3. Векторайзер готов работать с новыми текстами.
-"""
-X = []
-y = []
-for name in data:
-    for phrase in data[name]['examples']:
-        X.append(phrase)
-        y.append(name)
-    for phrase in data[name]['responses']:
-        X.append(phrase)
-        y.append(name)
-
-local_vectorize = CountVectorizer()
-local_vectorize.fit(X)
-X_vec = local_vectorize.transform(X)
-
-"""
-ШАГ 3. Обучаем классификатор текстов.
-"""
-# Создаём модель
-model = MLPClassifier()
-# Обучаем модель
-model.fit(X_vec, y)
-
-"""
-ШАГ 4. Оцениваем качество.
-
-Accuracy = (n угаданных/N всего) — доля правильных ответов (больше - лучше).
-"""
-model.score(X_vec, y)  # Качество на тренировочной выборке (accuracy)
+ # Скачаем файл с большим количеством примеров для обучения
+ url = "https://drive.google.com/uc?export=download&id=1VyvE5DGPqWFBHm5MFPUv-upq-SMZEXHx"
+ filename = "local_intents_dataset.json"
+ urllib.request.urlretrieve(url, filename)
 
 
-"""
-ШАГ 5. Пишем функцию для получения интента с помощью ML.
-"""
+ # Считываем файл в словарь
+ with open(filename, 'r', encoding='UTF-8') as file:
+     data = json.load(file)
 
 
-def get_intent(element):
-    text_vec = local_vectorize.transform([element])
-    intent = model.predict(text_vec)[0]
-    return intent
+ """
+ Пробегаем по всему словарю – берём пары ключ-значение и раскладываем по двум спискам.
+ Сначала берём пары name, intent.
+ Потом для каждого элемента в examples и responses кладём в массив X элемент,
+ Тем временем в y кладём name – название корневого интента.
+ """
+ X = []
+ y = []
+ for name in data:
+     for phrase in data[name]['examples']:
+         X.append(phrase)
+         y.append(name)
+     for phrase in data[name]['responses']:
+         X.append(phrase)
+         y.append(name)
 
 
-def get_response(intent):
-    return random.choice(data[intent]['responses'])
-
-
-def bot(phrase_from):
-    intent = get_intent(phrase_from)
-    answer = get_response(intent)
-    return answer
-
-
-# Запуск, чтобы пообщаться
-text = ""
-while get_intent(text) != 'bye':
-    text = input('< ')
-    print('> ', bot(text))
+ local_vectorize = CountVectorizer()
+ local_vectorize.fit(X)
+ X_vec = local_vectorize.transform(X)
