81eaab19762a64bd2fb0b219e40b6fea76530e7d
prairieson <114734911+prairieson@users.noreply.github.com>
2022-10-17 21:03:47 +0300

    Bot with my ml model for fun

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ import urllib.request  # Для работы со ссылками
 import json  # Для работы с json
 import random
 from sklearn.feature_extraction.text import CountVectorizer  # Для обучения векторайзера (простенького ML)
-from sklearn.neural_network import MLPClassifier
+import nest_asyncio
+from telegram import Update  # update for get information from server (new messages, new contacts)
+from telegram.ext import ApplicationBuilder  # Tool for creating and customizing an application
+from telegram.ext import MessageHandler  # Tool to respond to user action
+from telegram.ext import filters
+import pickle
 
 """
-ШАГ 1. Собираем данные, на основании которых будем обучать.
+ШАГ 1. Загружаем обученную модель.
+"""
+# Скачаем файл с обученным сетом
+url = "https://drive.google.com/uc?export=download&id=1zXL6iGZKgJGQPv_IO1Dqgt_vIkyewBJD"
+filename = "model.sav"
+urllib.request.urlretrieve(url, filename)
 
-1. Фраза на вход => Модель предсказывает интент фразы.
-2. Входные данные (Фразы, X) Выходные данные (Интенты, y).
-3. Модель обучится на наших примерах и сможет предсказывать интенты по фразе.
+# С помощью pickle распаковываю модель
+with open(filename, 'br') as file:
+    model = pickle.load(file)
+
+"""
+ШАГ 2. Собираем данные, на основании которых будем отвечать.
 """
 # Скачаем файл с большим количеством примеров для обучения
 url = "https://drive.google.com/uc?export=download&id=1VyvE5DGPqWFBHm5MFPUv-upq-SMZEXHx"
@@ -24,14 +37,9 @@ urllib.request.urlretrieve(url, filename)
 with open(filename, 'r', encoding='UTF-8') as file:
     data = json.load(file)
 
-
 """
-ШАГ 2. Преобразуем слова в числа, набор фраз в вектор.
-
-1. На вход подается большой набор фраз.
-2. Векторайзер обучается (каждому слову выделяется отдельная ячейка, 
-    куда будет вставляться код - число, сколько раз слово встретилось во фразе).
-3. Векторайзер готов работать с новыми текстами.
+ШАГ 3. Преобразуем слова в числа, набор фраз в вектор.
+Это нужно для того, чтобы обученная модель могла выбирать из чего ей отвечать.
 """
 X = []
 y = []
@@ -48,20 +56,11 @@ local_vectorize.fit(X)
 X_vec = local_vectorize.transform(X)
 
 """
-ШАГ 3. Обучаем классификатор текстов.
-"""
-# Создаём модель
-model = MLPClassifier()
-# Обучаем модель
-model.fit(X_vec, y)
-
-"""
 ШАГ 4. Оцениваем качество.
-
+Это нужно при запуске для контроля качества.
 Accuracy = (n угаданных/N всего) — доля правильных ответов (больше - лучше).
 """
 model.score(X_vec, y)  # Качество на тренировочной выборке (accuracy)
-
 
 """
 ШАГ 5. Пишем функцию для получения интента с помощью ML.
@@ -83,9 +82,43 @@ def bot(phrase_from):
     answer = get_response(intent)
     return answer
 
+"""
+ШАГ 6. Put the secret_file.txt file with the token from the bot in the project folder.
+"""
+with open('secret_file.txt', 'r') as f:
+    TOKEN = f.read()
 
-# Запуск, чтобы пообщаться
-text = ""
-while get_intent(text) != 'bye':
-    text = input('< ')
-    print('> ', bot(text))
+
+"""
+ШАГ 7. Обучаем бота взаимодействовать.
+
+1. Фраза на вход => Модель предсказывает интент фразы.
+2. Входные данные (Фразы, X) Выходные данные (Интенты, y).
+3. Модель обучится на наших примерах и сможет предсказывать интенты по фразе.
+"""
+nest_asyncio.apply()  # Для того чтобы функция bot могла работать асинхронно: для нескольких пользователей сразу
+
+
+# Функция для MessageHandler'a: вызывать её при каждом сообщении бота
+async def reply(update: Update, context) -> None:
+    user_text = update.message.text
+    reply_for_user = bot(user_text)
+    print('<', user_text)
+    print('>', reply_for_user)
+
+    pass  # Ответ пользователю
+
+    await update.message.reply_text(reply_for_user)
+
+
+# Создаём объект приложения – связываем его с токеном
+app = ApplicationBuilder().token(TOKEN).build()
+
+# Создаём обработчик текстовых сообщений
+handler = MessageHandler(filters.Text(), reply)
+
+# Добавляем обработчик в приложение
+app.add_handler(handler)
+
+# Запускаем приложение: бот крутится
+app.run_polling()
